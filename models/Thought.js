da47abb19866914655ccf4a88a11e077d82470db
besharpe5 <besharpe15@gmail.com>
2022-04-09 13:22:32 -0400

    added User model

--- models/Thought.js
+++ models/Thought.js
@@ -57,6 +57,7 @@ const thoughtSchema = new Schema (
     }
 );
 
+// retrieves the length of the thought's reactions array field on query
 thoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
 });
